12e2a0611ebf468dcf5278b24a59375850824d5b
Andrew Trick <andyjtrick@gmail.com>
2019-01-21 09:44:51 -0600

    lab three setup.

---
+++ week_three/lab_3-2.R
@@ -0,0 +1,30 @@
+# Titanic Survivor EDA
+
+library(ggplot2)
+
+#########################
+# Setup and data import #
+#########################
+
+setwd('E://projects/it460/week_three/')
+
+df <- read.csv('../data/titanic.csv')
+
+############
+# Cleaning #
+############
+
+################
+# Text Reports #
+################
+
+###########
+# Visuals #
+###########
+
+
+
+# Required commands
+# - what percent of passengers survived           / text report and table and bars
+# - What is average age of passengers             / histogram and text report
+# - show relationship between age and fare paid   / scatter plot w/ trend line & report
