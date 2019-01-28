acc9ab9cbcbbc1118a5af2dbcc5ae7b3e505c17d
Andrew Trick <andyjtrick@gmail.com>
2019-01-28 10:30:29 -0600

    preproc done.

--- week_four/lab_4-2.R
+++ week_four/lab_4-2.R
@@ -30,13 +30,29 @@ table(df$default)
 # process data #
 ################
 
-#transform to same format as textbook
-df$default <- ifelse(df$default == 2, 'yes', 'no') 
+# Transform to same format as textbook
+df$default <- ifelse(df$default > 1, 'yes', 'no') 
 
 
+# Data Prep
+set.seed(123)
+train <- sample(1000, 900)
 
-#######
-# EDA #
-#######
+# Set train and test data
+X_train <- df[train, ]
+y_train <- df[train, c('default')]
+
+X_test <- df[-train, ]
+y_train <- df[-train, c('default')]
+
+
+# Ensure poroportions of class are even between sets
+prop.table(table(X_train$default))
+prop.table(table(X_test$default))
+
+
+############
+# Modeling #
+############
 
 
