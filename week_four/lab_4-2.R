27338828b0394e69a2b0354a9d6466745e05c839
Andrew Trick <andyjtrick@gmail.com>
2019-01-28 11:46:44 -0600

    c5 and ada10 eval.

--- week_four/lab_4-2.R
+++ week_four/lab_4-2.R
@@ -2,6 +2,10 @@
 # Library Imports #
 ###################
 library(caret)
+library(C50)
+library(gmodels) 
+library(ROCR)
+
 
 #########################
 # Setup and data import #
@@ -40,19 +44,112 @@ train <- sample(1000, 900)
 
 # Set train and test data
 X_train <- df[train, ]
+X_train$default <- NULL
+
 y_train <- df[train, c('default')]
+y_train <- data.frame(y_train)
+colnames(y_train) <- "default"
 
 X_test <- df[-train, ]
-y_train <- df[-train, c('default')]
+X_test$default <- NULL
 
+y_test <- df[-train, c('default')]
+y_test <- data.frame(y_test)
+colnames(y_test) <- "default"
 
 # Ensure poroportions of class are even between sets
-prop.table(table(X_train$default))
-prop.table(table(X_test$default))
+prop.table(table(y_train))
+prop.table(table(y_test))
 
 
 ############
 # Modeling #
 ############
 
+######
+# C5 #    Acc - 74%  ,  Kappa - 35%,   auc - 67%
+######
 
+#train
+c5_model <- C5.0(X_train, y_train$default)
+
+# Model Results
+summary(c5_model)
+
+# Predict
+c5_pred <- predict(c5_model, X_test)
+
+# probability
+c5_prob <-data.frame(predict(c5_model, X_test, type = 'prob'))
+  
+
+# Confusion Matrix
+CrossTable(y_test$default, c5_pred,
+           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
+           dnn = c('actual default', 'predicted default'))
+
+
+###############
+# AdaBoost 10 #   Acc - 76% , Kappa - 42%,  auc - 78%
+###############
+
+# Train
+ada10_model <- C5.0(X_train, y_train$default, trials = 10)
+
+# Summary
+summary(ada10_model)
+
+# Predict
+ada10_pred <- predict(ada10_model, X_test)
+
+# probability
+ada10_prob <-data.frame(predict(ada10_model, X_test, type = 'prob'))
+
+# Confusion Matrix
+CrossTable(y_test$default, ada10_pred,
+           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
+           dnn = c('actual default', 'predicted default'))
+
+
+######################
+# Further Evaluation #
+######################
+######
+# C5 #
+######
+
+# matrix
+confusionMatrix(c5_pred, y_test$default, positive = "no")
+
+# ROC
+c5_roc_pred <- prediction(predictions = c5_prob$yes, labels = y_test)
+c5_perf <- performance(c5_roc_pred, measure = 'tpr', x.measure = 'fpr')
+
+# plot
+plot(c5_perf, main = "ROC surve for C5 model of defaults",
+     col = "blue", lwd = 3)
+abline(a = 0, b = 1, lwd = 2, lty = 2)
+
+# AUC
+c5_perf.auc <- performance(c5_roc_pred, measure = "auc")
+unlist(c5_perf.auc@y.values)
+
+#########
+# Ada10 #
+#########
+
+# matrix
+confusionMatrix(ada10_pred, y_test$default, positive = "no")
+
+#ROC
+ada10_roc_pred <- prediction(predictions = ada10_prob$yes, labels = y_test)
+ada10_perf <- performance(ada10_roc_pred, measure = 'tpr', x.measure = 'fpr')
+
+# plot
+plot(ada10_perf, main = "ROC surve for ada10 model of defaults",
+     col = "blue", lwd = 3)
+abline(a = 0, b = 1, lwd = 2, lty = 2)
+
+# AUC
+ada10_perf.auc <- performance(ada10_roc_pred, measure = "auc")
+unlist(ada10_perf.auc@y.values)
