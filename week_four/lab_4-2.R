1d530160c61c230ad44cef0267bfa9cff4ab8bd5
Andrew Trick <andyjtrick@gmail.com>
2019-01-28 08:23:01 -0600

    setup week four.

---
+++ week_four/lab_4-2.R
@@ -0,0 +1,16 @@
+###################
+# Library Imports #
+###################
+
+
+#########################
+# Setup and data import #
+#########################
+
+setwd('E://projects/it460/week_four/')
+
+df <- read.csv('../data/credit.csv')
+
+################
+# Examine data #
+################
