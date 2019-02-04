867e5743fd884112c691da39c84ff58423558cf2
Andrew Trick <andyjtrick@gmail.com>
2019-02-04 17:17:38 -0600

    set up final ANN envir.

---
+++ final/ANN.R
@@ -0,0 +1,27 @@
+# Fit ANN to Processed Data
+library(neuralnet)
+
+# import cleaned data
+df <- read.csv("../data/final/ad_cleaned.csv")
+
+# check
+head(df[1:6])
+
+##############
+# Split Data #
+##############
+
+
+#############
+# Fit Model #
+#############
+
+
+##############
+# Validation #
+##############
+
+
+###########
+# Results #
+###########
