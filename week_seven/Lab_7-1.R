40a21b579c3371d7fadcd2a4d9723af36369907c
Andrew Trick <andyjtrick@gmail.com>
2019-02-19 15:59:39 -0600

    ANN first round fin.

--- week_seven/Lab_7-1.R
+++ week_seven/Lab_7-1.R
@@ -1,7 +1,7 @@
 library(neuralnet)
 library(caret)
 
-
+set.seed(42)
 setwd("E:/projects/it460/week_seven")
 
 # import data
@@ -27,7 +27,7 @@ summary(concrete$strength)
 ##############
 # Split Data #
 ##############
-
+# ~70/30 split
 concrete_train <- concrete_norm[1:773, ]
 concrete_test <- concrete_norm[774: 1030, ]
 
@@ -35,5 +35,36 @@ concrete_test <- concrete_norm[774: 1030, ]
 #################
 # model fitting #
 #################
+ # fit neuralnet ann
+concrete_model <- neuralnet(strength ~ cement + slag + ash + water + 
+                 superplastic + coarseagg + fineagg + age, 
+               data = concrete_train)
 
-m <- 
+# visualize
+plot(concrete_model)
+
+# evaluate
+model_results <- compute(concrete_model, concrete_test[1:8])
+pred_str <- model_results$net.result
+
+# correlation
+cor(pred_str, concrete_test$strength)
+
+
+#################
+# Improve Model #
+#################
+# 5 hidden neurals in one layer
+concrete_model2 <- neuralnet(strength ~ cement + slag + ash + water +
+                               superplastic + coarseagg + fineagg + age,
+                             data = concrete_train, hidden = 5)
+
+# vis
+plot(concrete_model2)
+
+# evaluate
+model_results2 <- compute(concrete_model2, concrete_test[1:8])
+pred_str2 <- model_results2$net.result
+
+# correlation
+cor(pred_str2, concrete_test$strength)
